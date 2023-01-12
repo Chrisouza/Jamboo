aecfef56ace2435ac65c33733bd1b53d84777939
Richardson Christian <rizinhof5@gmail.com>
2023-01-12 15:21:34 -0300

    ultimo teste statics

--- arquivos/urls.py
+++ arquivos/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.index, name="home"),
     path('add-file', views.add_file, name="add-file"),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('registre/', views.registre, name="registre"),
     path('sair/', views.sair, name="sair"),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
