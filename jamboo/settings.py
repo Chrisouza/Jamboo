71134854c6727f2e3877c34e8e394ec3ed2aca1a
Richardson Christian <rcsc3w@yahoo.com.br>
2023-03-10 10:09:31 -0300

    nova origin corrigida

--- jamboo/settings.py
+++ jamboo/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-5^iyk!h)k5_*o3^kief+tvfw^%g@((^7ljoy^44p9rd8@vii@7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*", "https://projetojamboo.chrisouza.repl.co"]
-CSRF_TRUSTED_ORIGINS = ['https://projetojamboo.chrisouza.repl.co']
+ALLOWED_HOSTS = ["*", "https://jamboo.chrisouza.repl.co"]
+CSRF_TRUSTED_ORIGINS = ['https://jamboo.chrisouza.repl.co']
 
 
 # Application definition
