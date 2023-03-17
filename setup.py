c5e8edfc9b679fff56c0af738d2f4282e791fcf8
Richardson Christian <rcsc3w@yahoo.com.br>
2023-03-17 15:38:53 -0300

    migrando o sistema

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 import os
 
-if not os.path.exists("media"):
-    os.mkdir("media")
+folders = ['media', 'staticfiles']
+for folder in folders:
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+
+os.system("./rm")
+os.system("./migrate")
+if input("deseja criar um super usuario? s/n\n") == "s":
+    os.system("python3 manage.py createsuperuser")
