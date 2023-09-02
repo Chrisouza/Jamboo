1956fc89f6e375cfbb101ec04d2fe164a47f0606
Richardson Christian <rcsc3w@yahoo.com.br>
2023-09-01 22:32:44 -0300

    settings modify

--- jamboo/settings.py
+++ jamboo/settings.py
@@ -144,10 +144,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # informacoes de envio de email
-EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
-DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
-EMAIL_PORT = os.environ.get("EMAIL_PORT")
-EMAIL_HOST = os.environ.get("EMAIL_HOST")
+# EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
+# DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
+# EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+# EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
+# EMAIL_PORT = os.environ.get("EMAIL_PORT")
+# EMAIL_HOST = os.environ.get("EMAIL_HOST")
